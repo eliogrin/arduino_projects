558dae92f526f587b067a4b437249e3f13c65ac9
Illia Grynov <eliogrin@gmail.com>
2017-01-18 23:20:52 +0100

    flashing_christmas_lights - refactoring of loop method, added 3 more programm

--- flashing_christmas_lights/flashing_christmas_lights.ino
+++ flashing_christmas_lights/flashing_christmas_lights.ino
@@ -1,142 +1,237 @@
-#define REDPIN 5
-#define GREENPIN 6
-#define BLUEPIN 3
-#define BUTTON 2
+// Pins configuration:
+#define RED_PIN 5
+#define GREEN_PIN 6
+#define BLUE_PIN 3
+#define BUTTON_PIN 2
 
-int r, g, b;
-int fadeSpeed;
-int mode;
-int maxMode = 3;
+// Constants:
+#define MAX 255
+#define MIN 1
+
+// Global variables:
 volatile boolean changeMode;
+volatile boolean buttonPressed;
+int mode;
+int prev_red;
+int prev_green;
+int prev_blue;
 
 void setup() {
-  Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(BUTTON), buttonClick, RISING);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonClick, RISING); // allowing interraption by pressing button
   
-  pinMode(REDPIN, OUTPUT);
-  pinMode(GREENPIN, OUTPUT);
-  pinMode(BLUEPIN, OUTPUT);
-  
-  fadeSpeed = 10;
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+
+  // set starting data
   changeMode = false;
+  buttonPressed = false;
   mode = 1;
 }
  
  
 void loop() {
-  Serial.print("current mode ");
-  Serial.println(mode);
-  if (mode == 1) {
-    fadeSpeed = 10;
-    program1();
+  switch (mode) {
+    case 1:
+      program1(12);
+      break;
+    case 2:
+      program2(12);
+      break;
+    case 3:
+      program3(12);
+      break;
+    case 4:
+      program4(12);
+      break;
+    case 5:
+      program5(12);
+      break;
+    default:
+      mode = 1;
+      break;
   }
-  else if (mode == 2) {
-    fadeSpeed = 4;
-    program1();
-  }
-  else if (mode == 3) {
-    program2();
-  }
-  else {
-    mode = 1;
-  }
+
   if (changeMode) {
     mode++;
     changeMode = false;
-    Serial.print("-- switch mode to ");
-    Serial.println(mode);
-    analogWrite(REDPIN, 256);
-    analogWrite(BLUEPIN, 0);
-    analogWrite(GREENPIN, 0);
-    delay(1000);
-    analogWrite(REDPIN, 0);
-    analogWrite(BLUEPIN, 0);
-    analogWrite(GREENPIN, 0);
+    blink(mode);
+    prev_red = 1;
+    prev_green = 1;
+    prev_blue = 1;
+    buttonPressed = false;
   }
+}
+
+void program1(int timeout) {
+  growBright(RED_PIN, timeout); // from blue to violet
+  fadeBright(BLUE_PIN, timeout); // from violet to red
+  growBright(GREEN_PIN, timeout); // from red to yellow
+  fadeBright(RED_PIN, timeout); // from yellow to green
+  growBright(BLUE_PIN, timeout); // from green to teal
+  fadeBright(GREEN_PIN, timeout); // from teal to blue
+}
+
+void program2(int timeout) {
+  int red = getRandomBright();
+  int green = getRandomBright();
+  int blue = getRandomBright();
+  slideBrights(red, green, blue, timeout);
+  interruptibleDelay(timeout, 20);
+}
+
+void program3(int timeout) {
+  int red = getRandomBright();
+  int green = getRandomBright();
+  int blue = getRandomBright();
+  growBrights(red, green, blue, timeout);
+  interruptibleDelay(timeout, 30);
+  fadeBrights(red, green, blue, timeout);
+  interruptibleDelay(timeout, 30);
+}
+
+void program4(int timeout) {
+  int red = getRandomBright();
+  int green = getRandomBright();
+  int blue = getRandomBright();
+  setBrights(red, green, blue);
+  interruptibleDelay(timeout, 60);
+  turnOffBrights();
+  interruptibleDelay(timeout, 40);
+}
+
+void program5(int timeout) {
+  int red = getRandomBright();
+  int green = getRandomBright();
+  int blue = getRandomBright();
+  slideBrights(red, green, blue, timeout);
+  interruptibleDelay(timeout, 60);
+  slideBrights(0, 0, 0, timeout);
+  interruptibleDelay(timeout, 80);
 }
 
 void buttonClick() {
-  changeMode = true;
+  if (!buttonPressed) changeMode = true;
+  buttonPressed = true;
 }
 
-void program1() {
-  // fade from blue to violet
-  for (r = 0; r < 256; r++) { 
-    if (changeMode) {
-      break;
-    }
-    analogWrite(REDPIN, r);
-    delay(fadeSpeed);
-  } 
-  // fade from violet to red
-  for (b = 255; b > 0; b--) { 
-    if (changeMode) {
-      break;
-    }
-    analogWrite(BLUEPIN, b);
-    delay(fadeSpeed);
-  } 
-  // fade from red to yellow
-  for (g = 0; g < 256; g++) { 
-    if (changeMode) {
-      break;
-    }
-    analogWrite(GREENPIN, g);
-    delay(fadeSpeed);
-  } 
-  // fade from yellow to green
-  for (r = 255; r > 0; r--) { 
-    if (changeMode) {
-      break;
-    }
-    analogWrite(REDPIN, r);
-    delay(fadeSpeed);
-  } 
-  // fade from green to teal
-  for (b = 0; b < 256; b++) { 
-    if (changeMode) {
-      break;
-    }
-    analogWrite(BLUEPIN, b);
-    delay(fadeSpeed);
-  } 
-  // fade from teal to blue
-  for (g = 255; g > 0; g--) { 
-    if (changeMode) {
-      break;
-    }
-    analogWrite(GREENPIN, g);
-    delay(fadeSpeed);
-  } 
-}
-
-void program2() {
-  int color1 = random(257);
-  Serial.print("\t1->");
-  Serial.print(color1);
-  int color2 = random(257);
-  Serial.print("\t2->");
-  Serial.println(color2);
-  int color3 = random(257);
-  Serial.print("\t3->");
-  Serial.println(color3);
-  for (int grow = 0; grow < 256; grow++) {
-    if (changeMode) {
-      break;
-    }
-    int current1 = map(grow, 0, color1, 0, 256);
-    Serial.print("\t\t1->");
-    Serial.println(current1);
-    int current2 = map(grow, 0, color2, 0, 256);
-    Serial.print("\t\t2->");
-    Serial.println(current2);
-    int current3 = map(grow, 0, color3, 0, 256);
-    Serial.print("\t\t3->");
-    Serial.println(current3);
-    analogWrite(REDPIN, current1);
-    analogWrite(BLUEPIN, current2);
-    analogWrite(GREENPIN, current3);
-    delay(10);
+void blink(int count) {
+  turnOffBrights();
+  delay(500);
+  for(int cycle = 1; cycle < count; cycle ++) {
+    setRed(MAX);
+    delay(300);
+    turnOffBrights();
+    delay(300);
   }
 }
 
+void growBright(int pin, int timeout) {
+  for (int grow = MIN; grow <= MAX; grow++) { 
+    if (changeMode) {
+      break;
+    }
+    setBright(pin, grow);
+    delay(timeout);
+  } 
+}
+
+void fadeBright(int pin, int timeout) {
+  for (int bright = MAX; bright >= MIN; bright--) { 
+    if (changeMode) {
+      break;
+    }
+    setBright(pin, bright);
+    delay(timeout);
+  } 
+}
+
+void slideBrights(int red, int green, int blue, int timeout) {
+  for (int slide = 0; slide <= 100; slide++) {
+    if (changeMode) {
+      break;
+    }
+    int redStep = map(slide, 0, 100, prev_red, red);
+    int greenStep = map(slide, 0, 100, prev_green, green);
+    int blueStep = map(slide, 0, 100, prev_blue, blue);
+    setRed(redStep);
+    setGreen(greenStep);
+    setBlue(blueStep);
+    delay(timeout);
+  }
+  prev_red = red;
+  prev_green = green;
+  prev_blue = blue;
+}
+
+void growBrights(int red, int green, int blue, int timeout) {
+  for (int grow = MIN; grow <= MAX; grow++) {
+    if (changeMode) {
+      break;
+    }
+    int redStep = map(grow, MIN, MAX, MIN, red);
+    int greenStep = map(grow, MIN, MAX, MIN, green);
+    int blueStep = map(grow, MIN, MAX, MIN, blue);
+    setRed(redStep);
+    setGreen(greenStep);
+    setBlue(blueStep);
+    delay(timeout);
+  }
+}
+
+void fadeBrights(int red, int green, int blue, int timeout) {
+  for (int fade = MAX; fade >= MIN; fade--) {
+    if (changeMode) {
+      break;
+    }
+    int redStep = map(fade, MIN, MAX, MIN, red);
+    int greenStep = map(fade, MIN, MAX, MIN, green);
+    int blueStep = map(fade, MIN, MAX, MIN, blue);
+    setRed(redStep);
+    setGreen(greenStep);
+    setBlue(blueStep);
+    delay(timeout);
+  }
+}
+
+void setRed(int bright) {
+  setBright(RED_PIN, bright);
+}
+
+void setGreen(int bright) {
+  setBright(GREEN_PIN, bright);
+}
+
+void setBlue(int bright) {
+  setBright(BLUE_PIN, bright);
+}
+
+void setBright(int pin, int bright) {
+  analogWrite(pin, bright);
+}
+
+void setBrights(int red, int green, int blue) {
+  setRed(red);
+  setGreen(green);
+  setBlue(blue);
+}
+
+void turnOffBrights() {
+  setBrights(0, 0, 0);
+}
+
+int getRandomBright() {
+  int result = random(MIN, MAX);
+  return result;
+}
+
+void interruptibleDelay(int timeout, int cycles) {
+  int x = 0;
+  do {
+    if (changeMode) {
+      break;
+    }
+    delay(timeout);
+    x++;
+  } while (x < cycles);
+}
